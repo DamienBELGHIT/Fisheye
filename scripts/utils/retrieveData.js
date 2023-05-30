109b9b1b061ac783d35eeec5f96f637ff83adcce
Damien BELGHIT <damien.belghit@yahoo.fr>
2023-05-30 17:00:03 +0200

    Replace local fetch with global fetch

--- scripts/utils/retrieveData.js
+++ scripts/utils/retrieveData.js
@@ -1,6 +1,6 @@
 //récupère les données json des photographes
 export async function getPhotographers() {
-    const json = fetch("../../data/photographers.json")
+    const json = fetch("/data/photographers.json")
     .then(res => res.json())
     .then((data)=> {return data;})
     return json;
